88ab9d134eae7daed7f5c5f60b720a1fcfb48334
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2021-10-24 16:25:16 +0300

    logo added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="wrapper">
+      <header>
+        <div className="headerLeft">
+          <img src="./img/logo.png" />
+          <div className="headerInfo">
+            <h3>React Sneakers</h3>
+            <p>Магазин лучших кросссовок</p>
+          </div>
+        </div>
+        <ul className="headerRight">
+          <li>
+            <svg />
+            <span>1205 грн.</span>
+          </li>
+          <li>
+            <svg />
+          </li>
+        </ul>
+      </header>
+      <div className="content">
+        <h1>Все кросссовки</h1>
+        ...
+      </div>
+    </div>
+  );
 }
 
 export default App;
